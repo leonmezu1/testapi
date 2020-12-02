b42f5adb6dfd5ad8115d18dbdf8ceae3e3a1f9e2
leonmezu1 <leo.mez@outlook.com>
2020-12-01 19:57:19 -0500

    Added pagination through kaminari using TDD
    Tests are passing

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :slug, presence: true, uniqueness: true
+
+  scope :recent, -> { order(created_at: :desc) }
+  scope :older, -> { order(created_at: :asc) }
 end
